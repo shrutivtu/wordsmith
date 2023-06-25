e8e6b76502cbe85862d86979e283133ae334c763
shrutivtu <shrutisaxena1596@gmail.com>
2023-06-25 18:07:59 +0530

    fix: category routing fix

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,5 +1,6 @@
+"use client"
 import React from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { getCategories, getCategoryPost } from '@/services';
 import { PostCard, Categories, Loader } from '@/components';
 
@@ -17,9 +18,9 @@ const CategoryPost = async ({ params }: any) => {
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post, index) => (
-            <PostCard key={index} post={post.node} />
-          ))}
+          {posts.map((post, index) => {
+            return <PostCard key={index} post={post.node} />
+          })}
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative lg:sticky top-8">
